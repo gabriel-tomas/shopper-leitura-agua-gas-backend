c5d5d26e63a2268d63118a7bac775821b8fabb21
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-08-28 12:42:04 -0300

    feature: wait-for-it.sh file to wait db to connect and install his dependencies in docker-compose app command

---
+++ wait-for-db.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# wait-for-it.sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+clear
+
+set -e
+
+host="$1"
+port="$2"
+shift 2
+cmd="$@"
+
+until nc -z "$host" "$port"; do
+  >&2 echo -e "${YELLOW}Esperando conexão com banco de dados MySQL...${NC}"
+  clear
+  sleep 1
+done
+
+>&2 echo -e "${GREEN}Banco de dados MySQL está pronto. Executando App...${NC}"
+exec $cmd

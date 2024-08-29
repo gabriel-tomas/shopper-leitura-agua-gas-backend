686bd81bb457e077c906d1756299ed70aedf6c3d
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-08-28 22:46:08 -0300

    model change: using gemini-1.5-flash, por que ele aceita mais requisições por dia

--- src/config/googleGenAi.ts
+++ src/config/googleGenAi.ts
@@ -2,4 +2,6 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
 
-export const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro' });
+const modelName = 'gemini-1.5-flash'
+
+export const model = genAI.getGenerativeModel({ model: modelName });

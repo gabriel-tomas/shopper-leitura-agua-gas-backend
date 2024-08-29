7f3a62886ca46b00b8ffd22bf092c7b7fdc09e05
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-08-28 22:47:28 -0300

    chore: código formatado

--- src/config/googleGenAi.ts
+++ src/config/googleGenAi.ts
@@ -2,6 +2,6 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
 
-const modelName = 'gemini-1.5-flash'
+const modelName = 'gemini-1.5-flash';
 
 export const model = genAI.getGenerativeModel({ model: modelName });
